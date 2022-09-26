c43a6e47b2cb54c61a2e90e2785eac64e0a1f2a7
Александр Ульченков <youlchenkov@icloud.com>
2022-09-26 17:14:33 +0300

    Сделанное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        countsLeapYear(2022);
+        getDeviceLink(0, 2005 );
+        int deliveryDays = calculateDeliveryDays(60);
+        System.out.println("Потребуется дней: " + deliveryDays);
+    }
+    public static void countsLeapYear(int year) {
+        boolean isLeap;
+        isLeap = (year % 400 == 0) || ((year % 100 != 0 && (year % 4 == 0)));
+        System.out.println(isLeap ? year + " год является високосным" : year + " год не является високосным");
+    }
+
+    public static void getDeviceLink(int osType, int manufacturingYear) {
+        if (osType != 1 && osType != 0) {
+            throw new IllegalArgumentException("Wrong OS type");
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (manufacturingYear > currentYear) {
+            throw new IllegalArgumentException("Wrong manufacturing year");
+        }
+        String osString = osType == 0 ? "iOS" : "Android";
+        String versionString = manufacturingYear > 2015 ? "полную" : "облегченную";
+        System.out.printf("Установите %s версию для %s по ссылке%n", versionString, osString);
+    }
+
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else {
+            return (int) Math.round((double) deliveryDistance / 40) + 1;
+        }
     }
 }
